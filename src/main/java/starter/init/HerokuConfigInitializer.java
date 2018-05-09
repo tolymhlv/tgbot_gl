3e6d34a2cdba51a3afafabfb600f98914253a5c2
tolymhlv <tolymhlv@gmail.com>
2018-05-09 17:34:42 +0300

    heroku test

--- src/main/java/starter/init/HerokuConfigInitializer.java
+++ src/main/java/starter/init/HerokuConfigInitializer.java
@@ -1,5 +1,9 @@
 package starter.init;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpHost;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -15,8 +19,10 @@ public class HerokuConfigInitializer implements ConfigInitializer {
    HashMap<String,String> config;
 
     public HerokuConfigInitializer() {
+        HttpGet hg = new HttpGet("https://api.heroku.com/apps/tg-bot-1/config-vars");
+        hg.setHeader("Accept", "application/vnd.heroku+json; version=3");
         try (CloseableHttpClient client = HttpClients.createDefault();
-             CloseableHttpResponse response = client.execute(new HttpGet("https://api.heroku.com/apps/tg-bot-1/config-vars"))) {
+             CloseableHttpResponse response = client.execute(hg)) {
             config = new HashMap<String, String>((Map<String, String>) response.getEntity());
             System.out.println(config);
         } catch (IOException e) {
